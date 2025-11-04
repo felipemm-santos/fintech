8a9a9f3e160245dc884cbb8d4fefe21874e983c8
felipemm-santos <tfelipesantos99s@gmail.com>
2025-11-04 00:02:03 -0300

    fix: add content to the header

--- final-project/frontend/src/components/Header.tsx
+++ final-project/frontend/src/components/Header.tsx
@@ -1,7 +1,19 @@
+import Link from "next/link";
+
 export default function Header() {
   return (
-    <header>
-      <h1>Fintech</h1>
+    <header className="bg-white shadow p-4">
+      <div className="container mx-auto flex items-center justify-evenly">
+        <Link href="/dashboard/expenses" className="font-bold text-2xl">
+          Fintech
+        </Link>
+        <nav className="flex items-center gap-4">
+          <Link href="/dashboard/expenses">Despesas</Link>
+          <Link href="/dashboard/incomes">Receitas</Link>
+          <Link href="/dashboard/investiments">Investimentos</Link>
+          <Link href="/dashboard/goals">Objetivos</Link>
+        </nav>
+      </div>
     </header>
   );
 }
